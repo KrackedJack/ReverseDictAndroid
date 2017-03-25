e9352a94734aee3ced40500a9ac43f11c748f075
prithvi <prithvi.c28@gmail.com>
2017-03-25 10:04:28 +0530

    Commit 2

--- app/src/main/java/io/fragmentationsalt/reversedictandroid/Home.java
+++ app/src/main/java/io/fragmentationsalt/reversedictandroid/Home.java
@@ -3,11 +3,30 @@ package io.fragmentationsalt.reversedictandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class Home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+    public void send()
+    {
+        Socket s = null;
+        String host = "10.0.4.60";
+        try {
+            s = new Socket(host,4030);
+            OutputStream out = s.getOutputStream();
+            String h = "hello";
+            out.write(Byte.valueOf(h));
+            out.close();
+            s.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
